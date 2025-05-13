7f5348a214767953d952780fb1ca44bd458979a1
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-13 16:04:32 -0300

    feat: add rate limitter

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,5 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ThrottlerModule } from "@nestjs/throttler";
 import { ArticleController } from "src/controllers/articles.controller";
 import { ControllersModule } from "src/controllers/controllers.module";
 import { env } from "src/env.config";
@@ -9,6 +10,14 @@ import { ErrorMiddleware } from "src/middlewares/error.middleware";
   imports: [
     MongooseModule.forRoot(env.APP_ATLAS_MONGODB_URL),
     ControllersModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
   ],
 })
 export class AppModule implements NestModule {
