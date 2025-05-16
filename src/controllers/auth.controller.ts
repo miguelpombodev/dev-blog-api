6b6e5d3446f4c089b74e114ee6100587d055c00e
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-15 23:19:39 -0300

    feat: send jwt token via cookie

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,8 @@
 import { IUserRequest } from "@abstractions/auth/user.interface";
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
 import { JwtService, JwtSignOptions } from "@nestjs/jwt";
 import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
+import { Response } from "express";
 import { env } from "src/env.config";
 @Controller({
   path: "/auth",
@@ -19,7 +19,13 @@ export class AuthController {
 
   @Get("github/callback")
   @UseGuards(AuthGuard("github"))
-  async githubCallback(@Req() request: IUserRequest) {
+  async githubCallback(
+    @Req() request: IUserRequest,
+    @Res({
+      passthrough: true,
+    })
+    response: Response,
+  ) {
     const user = request.user;
 
     const tokenPayload: { username: string } = {
@@ -31,6 +37,15 @@ export class AuthController {
       this.jwtOptions,
     );
 
-    return token;
+    const now = new Date();
+
+    response.cookie("loginCredentials", token, {
+      sameSite: true,
+      httpOnly: true,
+      maxAge: 6000,
+      expires: new Date(now.getTime() + 1000 * 36000),
+    });
+
+    return response.send();
   }
 }
