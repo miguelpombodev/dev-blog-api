d64afb7d3b981382c1ace3c41b3adffb8da4a12a
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-15 23:01:39 -0300

    feat: add jwt token creation by github login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,36 @@
-import { Controller } from "@nestjs/common";
-
+import { IUserRequest } from "@abstractions/auth/user.interface";
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { JwtService, JwtSignOptions } from "@nestjs/jwt";
+import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
+import { env } from "src/env.config";
 @Controller({
   path: "/auth",
 })
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly jwtService: JwtService) {}
+  private readonly jwtOptions: JwtSignOptions = {
+    secret: env.AUTH_SECRET,
+  };
+
+  @Get("github")
+  @UseGuards(AuthGuard("github"))
+  async githubLogin() {}
+
+  @Get("github/callback")
+  @UseGuards(AuthGuard("github"))
+  async githubCallback(@Req() request: IUserRequest) {
+    const user = request.user;
+
+    const tokenPayload: { username: string } = {
+      username: user.profile.username,
+    };
+
+    const token = await this.jwtService.signAsync(
+      tokenPayload,
+      this.jwtOptions,
+    );
+
+    return token;
+  }
+}
--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -5,9 +5,23 @@ import { AuthController } from "./auth.controller";
 import { HealthController } from "./health.controller";
 import { TerminusModule } from "@nestjs/terminus";
 import { HttpModule } from "@nestjs/axios";
+import { JwtModule } from "@nestjs/jwt";
+import { env } from "src/env.config";
+import { GithubStrategy } from "src/auth/github.strategy";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
-  imports: [ServicesModule, TerminusModule, HttpModule],
+  imports: [
+    PassportModule,
+    ServicesModule,
+    TerminusModule,
+    HttpModule,
+    JwtModule.register({
+      secret: env.AUTH_SECRET,
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [AuthController, ArticleController, HealthController],
+  providers: [GithubStrategy],
 })
 export class ControllersModule {}
