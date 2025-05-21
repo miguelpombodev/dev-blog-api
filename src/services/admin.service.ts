4b4ca549d0df0f6a851fb4f553ed2b794483001d
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-21 17:38:59 -0300

    feat: create endpoint to get articles management infos

---
+++ src/services/admin.service.ts
@@ -0,0 +1,52 @@
+import { Result } from "@abstractions/result";
+import {
+  GetAllArticlesAdmin,
+  GetCategoriesCount,
+} from "@dtos/output/getAllArticlesAdmin";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import ArticlesRepository from "@repositories/articles.repositories";
+import { Article } from "@schemas/article.schema";
+import { Model } from "mongoose";
+
+@Injectable()
+export class AdminService {
+  constructor(
+    private readonly _articleRepository: ArticlesRepository,
+    @InjectModel(Article.name) private articleModel: Model<Article>,
+  ) {}
+
+  async getAllArticles(): Promise<Result<GetAllArticlesAdmin>> {
+    const articles = await this._articleRepository.getAll();
+    const articlesCountByCategory = this.getArticlesCategoryCount(articles);
+
+    const result = GetAllArticlesAdmin.create(
+      articles.length,
+      articles,
+      articlesCountByCategory,
+    );
+
+    return Result.success<GetAllArticlesAdmin>(result);
+  }
+
+  private getArticlesCategoryCount(articles: Article[]): GetCategoriesCount[] {
+    const tagsCount: GetCategoriesCount[] = [];
+
+    articles.map((article) => {
+      article.tags.map((tag) => {
+        const checkTagAlreadyInList = tagsCount.find(
+          (c) => c.getTagName() == tag,
+        );
+
+        if (checkTagAlreadyInList) {
+          return checkTagAlreadyInList.setIncrementTag();
+        }
+
+        const categoryCount = GetCategoriesCount.create(tag, 1);
+        tagsCount.push(categoryCount);
+      });
+    });
+
+    return tagsCount;
+  }
+}
