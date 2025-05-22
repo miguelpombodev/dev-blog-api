830501ad78fe157729fb2dc88a618c4219962753
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-22 09:57:28 -0300

    fix: remove unused import

--- src/controllers/articles.controller.ts
+++ src/controllers/articles.controller.ts
@@ -5,7 +5,6 @@ import { GetBySlugSchema } from "@dtos/getArticleSchema";
 import { ZodValidationPipe } from "@pipes/zod-validation.pipe";
 import { Article } from "@schemas/article.schema";
 import { ArticleService } from "@services/articles.service";
-import { JwtAuthGuard } from "src/guards/jwtAuth.guard";
 
 @Controller({
   path: "/article",
