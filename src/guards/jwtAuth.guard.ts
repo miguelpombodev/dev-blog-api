dca8efd950331ce8c7c3b0e0c0eef1dcf6d6bcb6
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-28 09:57:28 -0300

    fix: fix how to validate cookies

--- src/guards/jwtAuth.guard.ts
+++ src/guards/jwtAuth.guard.ts
@@ -1,8 +1,14 @@
-import { CanActivate, ExecutionContext, Logger } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
 import { authConfig } from "src/auth/auth.config";
 
+@Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(private readonly jwtService: JwtService) {}
   private readonly _logger = new Logger(JwtAuthGuard.name, {
@@ -13,7 +19,7 @@ export class JwtAuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<Request>();
     const token = request.cookies["loginCredentials"] as string;
 
-    if (token) return false;
+    if (!token) return false;
 
     try {
       const payload: { username: string } = await this.jwtService.verifyAsync(
