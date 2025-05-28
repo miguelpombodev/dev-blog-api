174a8196367c98583893721986dabc2fb62222e1
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-28 16:36:38 -0300

    feat:  create new tag route and tag flow

--- src/schemas/tag.schema.ts
+++ src/schemas/tag.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { BaseEntity } from "./base_entity.schema";
+import { Document } from "mongoose";
 
 @Schema({ timestamps: true })
 export class Tag extends BaseEntity {
   @Prop({ required: true })
-  name: string;
+  title: string;
 
   @Prop({ required: true })
   color: string;
